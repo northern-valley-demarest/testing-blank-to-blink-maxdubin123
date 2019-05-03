a6b6000c1de05c855ea063821fc010bc3ca70931
maxdubin123 <50246142+maxdubin123@users.noreply.github.com>
2019-05-03 14:26:19 -0400

    Update starter_sketch.ino

--- starter_sketch.ino
+++ starter_sketch.ino
@@ -1,9 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinmode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWtie (13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
 }
